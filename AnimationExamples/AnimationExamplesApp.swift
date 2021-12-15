94fe91e29295434e01aa3ad4aff6c663af776869
Franka Keil <fkeil@lcs.on.ca>
2021-12-15 08:52:15 -0500

    Initial Commit

---
+++ AnimationExamples/AnimationExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationExamplesApp.swift
+//  AnimationExamples
+//
+//  Created by Franka Keil on 2021-12-15.
+//
+
+import SwiftUI
+
+@main
+struct AnimationExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationExamples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimationExamples
+//
+//  Created by Franka Keil on 2021-12-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
